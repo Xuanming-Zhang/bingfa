d55bdf59031b7b049edd53572e47bc82663ad368
Xuanming-Zhang <xuanming@usc.edu>
2022-07-18 10:10:25 +0800

    bingfa

---
+++ 01. multi_thred_craw.py
@@ -0,0 +1,41 @@
+import time
+
+import blog_spider
+import threading
+
+
+def time_decorator(method_name):
+    def outter(func):
+        def wrapper():
+            start = time.time()
+            print(method_name+" starts")
+            func()
+            print(method_name+" ends")
+            end = time.time()
+            print("takes", end - start, "seconds")
+        return wrapper
+    return outter
+
+
+@time_decorator("singel thread")
+def single_thread():
+    for url in blog_spider.urls:
+        blog_spider.craw(url)
+
+
+@time_decorator("multi thread")
+def multi_thread():
+    threads = []
+    for url in blog_spider.urls:
+        threads.append(threading.Thread(target=blog_spider.craw, args=(url,)))
+
+    for thread in threads:
+        thread.start()
+
+    for thread in threads:
+        thread.join()
+
+
+if __name__ == "__main__":
+    single_thread()
+    multi_thread()
---
+++ blog_spider.py
@@ -0,0 +1,22 @@
+import requests
+from bs4 import BeautifulSoup
+
+urls = [
+    f"https://www.cnblogs.com/#p{i}" for i in range(1, 51)
+]
+
+
+def craw(url):
+    r = requests.get(url)
+    return r.text
+
+
+def parse(html):
+    soup = BeautifulSoup(html, "html.parser")
+    links = soup.find_all("a", class_="post-item-title")
+    return [(link["href"], link.get_text()) for link in links]
+
+
+if __name__ == "__main__":
+    for result in parse(craw(urls[2])):
+        print(result)
